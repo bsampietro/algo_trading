d310cb7a54a542990c027688401ffe0722af6b1f
Bruno Sampietro <bsampietro@gmail.com>
2018-08-06 17:17:25 -0400

    Smarter speeding

--- models/speed.py
+++ models/speed.py
@@ -2,10 +2,8 @@ class Speed:
     def __init__(self, monitor):
         self.mtr = monitor
 
-        self.max_time_speed = []
-        self.min_time_speed = []
-        self.max_trend_speed = []
-        self.min_trend_speed = []
+        self.time_speed = []
+        self.trend_speed = []
         self.is_time_speeding = 0
         self.is_trend_speeding = 0
 
@@ -30,22 +28,6 @@ class Speed:
             return 'down'
 
 
-    def find_time_speeding(self, last, ticks):
-        if len(self.speeds) == 0:
-            return 0
-        price_data = self.mtr.data[-last:]
-        last_cdp = price_data[-1]
-        for cdp in reversed(price_data):
-            ticks_move = self.mtr.ticks((last_cdp.price - cdp.price))
-            if ticks_move == ticks:
-                time = last_cdp.time - cdp.time
-                if ticks not in self.speeds:
-                    continue
-                if time < self.speeds[ticks] * 1.20:
-                    return 1 if ticks > 0 else -1
-        return 0
-
-
     def update_speeds(self):
         self.update_time_speed()
         self.update_trend_speed()
@@ -55,38 +37,28 @@ class Speed:
         price_data = self.mtr.data_since(5)
         if len(price_data) < 2:
             return
+        self.is_time_speeding = 0
+        ticks = 0
         max_ticks = 0
-        min_ticks = 0
         for cdp in reversed(price_data):
             ticks = self.mtr.ticks((price_data[-1].price - cdp.price))
-            if ticks > max_ticks:
+            if abs(ticks) > abs(max_ticks):
                 max_ticks = ticks
-            elif ticks < min_ticks:
-                min_ticks = ticks
 
-        # Long
-        if len(self.max_time_speed) >= 2:
-            if self.max_time_speed[-1].time - self.max_time_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
-                if max_ticks > max(self.max_time_speed, key=lambda sp: sp.ticks).ticks * 1.20:
-                    self.is_time_speeding = 1
+        if len(self.time_speed) >= 2:
+            if self.time_speed[-1].time - self.time_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
+                if ticks > 0:
+                    if 5 < ticks > max(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                        self.is_time_speeding = ticks
+                elif ticks < 0:
+                    if -5 > ticks < min(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                        self.is_time_speeding = ticks
 
-            while self.max_time_speed[-1].time - self.max_time_speed[0].time > self.mtr.prm.primary_look_back_time:
-                self.max_time_speed.pop(0)
+            while self.time_speed[-1].time - self.time_speed[0].time > self.mtr.prm.primary_look_back_time:
+                self.time_speed.pop(0)
         
-        if max_ticks > 0:
-            self.max_time_speed.append(SpeedPoint(max_ticks, price_data[-1].time))
-
-        # Short
-        if len(self.min_time_speed) >= 2:
-            if self.min_time_speed[-1].time - self.min_time_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
-                if min_ticks < min(self.min_time_speed, key=lambda sp: sp.ticks).ticks * 1.20:
-                    self.is_time_speeding = -1
-
-            while self.min_time_speed[-1].time - self.min_time_speed[0].time > self.mtr.prm.primary_look_back_time:
-                self.min_time_speed.pop(0)
-
-        if min_ticks < 0:
-            self.min_time_speed.append(SpeedPoint(min_ticks, price_data[-1].time))
+        if ticks < -2 or ticks > 2:
+            self.time_speed.append(SpeedPoint(ticks, price_data[-1].time))
 
 
     def update_trend_speed(self):
@@ -94,68 +66,42 @@ class Speed:
         if len(price_data) < 2:
             return
         self.is_trend_speeding = 0
-        max_ticks = 0
-        min_ticks = 0
-        long_trend = price_data[-1].trend > 0
+        ticks = 0
         complete_iteration = True
         for i in reversed(range(len(price_data) - 1)):
-            if long_trend:
-                if price_data[i].trend < 0:
-                    max_ticks = price_data[-1].trend - price_data[i+1].trend
-                    complete_iteration = False
-                    break
-            else:
-                if price_data[i].trend > 0:
-                    min_ticks = price_data[-1].trend - price_data[i+1].trend
-                    complete_iteration = False
-                    break
-        if long_trend:
-            if complete_iteration:
-                max_ticks = price_data[-1].trend - price_data[0].trend
+            if (price_data[-1].trend > 0 and price_data[i].trend < 0 or 
+                    price_data[-1].trend < 0 and price_data[i].trend > 0):
+                ticks = price_data[-1].trend - price_data[i+1].trend
+                complete_iteration = False
+                break
+        if complete_iteration:
+            ticks = price_data[-1].trend - price_data[0].trend
 
-            if len(self.max_trend_speed) >= 2:
-                if self.max_trend_speed[-1].time - self.max_trend_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
-                    if max_ticks > max(self.max_trend_speed, key=lambda sp: sp.ticks).ticks * 1.20:
-                        self.is_trend_speeding = 1
+        if len(self.trend_speed) >= 2:
+            if self.trend_speed[-1].time - self.trend_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
+                if ticks > 0:
+                    if 5 < ticks > max(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                        self.is_trend_speeding = ticks
+                elif ticks < 0:
+                    if -5 > ticks < min(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                        self.is_trend_speeding = ticks
 
-                while self.max_trend_speed[-1].time - self.max_trend_speed[0].time > self.mtr.prm.primary_look_back_time:
-                    self.max_trend_speed.pop(0)
+            while self.trend_speed[-1].time - self.trend_speed[0].time > self.mtr.prm.primary_look_back_time:
+                self.trend_speed.pop(0)
 
-            if max_ticks > 0:
-                self.max_trend_speed.append(SpeedPoint(max_ticks, price_data[-1].time))
-        else:
-            if complete_iteration:
-                min_ticks = price_data[-1].trend - price_data[0].trend
-
-            if len(self.min_trend_speed) >= 2:
-                if self.min_trend_speed[-1].time - self.min_trend_speed[0].time > self.mtr.prm.primary_look_back_time - 200:
-                    if min_ticks < min(self.min_trend_speed, key=lambda sp: sp.ticks).ticks * 1.20:
-                        self.is_trend_speeding = -1
-                    
-                while self.min_trend_speed[-1].time - self.min_trend_speed[0].time > self.mtr.prm.primary_look_back_time:
-                    self.min_trend_speed.pop(0)
-
-            if min_ticks < 0:
-                self.min_trend_speed.append(SpeedPoint(min_ticks, price_data[-1].time))
+        if ticks < -2 or ticks > 2:
+            self.trend_speed.append(SpeedPoint(ticks, price_data[-1].time))
 
 
     def state_str(self):
         output = ""
-        output += "max_time_speed:\n"
-        for sp in self.max_time_speed:
-            output += f"{sp.state_str()}\n"
+        # output += "time_speed:\n"
+        # for sp in self.time_speed:
+        #     output += f"{sp.state_str()}\n"
 
-        output += "min_time_speed:\n"
-        for sp in self.min_time_speed:
-            output += f"{sp.state_str()}\n"
-
-        output += "max_trend_speed:\n"
-        for sp in self.max_trend_speed:
-            output += f"{sp.state_str()}\n"
-
-        output += "min_trend_speed:\n"
-        for sp in self.min_trend_speed:
-            output += f"{sp.state_str()}\n"
+        # output += "trend_speed:\n"
+        # for sp in self.trend_speed:
+        #     output += f"{sp.state_str()}\n"
 
         output += f"is_time_speeding: {self.is_time_speeding}\n"
         output += f"is_trend_speeding: {self.is_trend_speeding}\n"
