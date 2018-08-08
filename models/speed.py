46834180d224b71804509f8d46a4c04d3d9fb936
Bruno Sampietro <bsampietro@gmail.com>
2018-08-08 12:48:11 -0400

    Added lots of speed info

--- models/speed.py
+++ models/speed.py
@@ -3,9 +3,7 @@ class Speed:
         self.mtr = monitor
 
         self.time_speed = []
-        self.trend_speed = []
-        self.time_speeding_ticks = 0
-        self.trend_speeding_ticks = 0
+        self.time_speeding_points = []
 
         self._speed_min_time_passed = False
         self._last_time_speeding_time = 0
@@ -33,107 +31,135 @@ class Speed:
 
     def update_speeds(self):
         self.update_time_speed()
-        # self.update_trend_speed()
 
 
     def update_time_speed(self):
-        self.time_speeding_ticks = 0
+        # Get latest price data
         time_or_duration = self.mtr.prm.speeding_time
-        if self.mtr.data[-1].time - self._last_time_speeding_time < self.mtr.prm.speeding_time:
-            time_or_duration = self._last_time_speeding_time
+        if self._last_time_speeding_time > 0:
+            if self.mtr.data[-1].time - self._last_time_speeding_time > self.mtr.prm.speeding_time / 2.0:
+                time_or_duration = self._last_time_speeding_time
+            else:
+                return
         price_data = self.mtr.data_since(time_or_duration)
-        if len(price_data) < 2:
+
+        if len(price_data) <= 1:
+            self._last_time_speeding_time = 0
+            self.time_speeding_points = []
             return
+
+        # Process latest price data
         max_ticks = 0
         for cdp in reversed(price_data):
             ticks = self.mtr.ticks((price_data[-1].price - cdp.price))
             if abs(ticks) > abs(max_ticks):
                 max_ticks = ticks
 
-        if max_ticks <= -5 or max_ticks >= 5:
-            self.time_speed.append(SpeedPoint(max_ticks, price_data[-1].time))
+        # Create Speed point
+        speed_point = SpeedPoint(
+            ticks = max_ticks,
+            time = price_data[-1].time,
+            changes = len(price_data),
+            max_jump = max(price_data, key=lambda cdp: abs(cdp.jump)).jump
+        )
 
+        # Add to time speed
+        if speed_point.ticks <= -4 or speed_point.ticks >= 4:
+            self.time_speed.append(speed_point)
+
+        # Return if not enough info
         if len(self.time_speed) == 0:
             return
-
         if (price_data[-1].time - self.time_speed[0].time < self.mtr.prm.primary_look_back_time - 300 and
                 not self._speed_min_time_passed):
             return
         self._speed_min_time_passed = True
 
+        # Remove old data
         while price_data[-1].time - self.time_speed[0].time > self.mtr.prm.primary_look_back_time:
             self.time_speed.pop(0)
 
-        if 5 <= max_ticks >= max(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+        # Current decision
+        if len(self.time_speeding_points) == 0:
+            if 4 <= speed_point.ticks >= max(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                self._last_time_speeding_time = price_data[-1].time
+                self.time_speeding_points = [speed_point]
+            elif -4 >= speed_point.ticks <= min(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+                self._last_time_speeding_time = price_data[-1].time
+                self.time_speeding_points = [speed_point]
+        elif len(self.time_speeding_points) <= 4:
             self._last_time_speeding_time = price_data[-1].time
-            self.time_speeding_ticks = max_ticks
-        elif -5 >= ticks <= min(self.time_speed, key=lambda sp: sp.ticks).ticks * 0.75:
-            self._last_time_speeding_time = price_data[-1].time
-            self.time_speeding_ticks = max_ticks
+            self.time_speeding_points.append(speed_point)
+        else:
+            self._last_time_speeding_time = 0
+            self.time_speeding_points = []
 
 
-    def update_trend_speed(self):
-        self.trend_speeding_ticks = 0
-        price_data = self.mtr.data_since(5)
-        if len(price_data) < 2:
-            return
-        ticks = 0
-        complete_iteration = True
-        for i in reversed(range(len(price_data) - 1)):
-            if (price_data[-1].trend > 0 and price_data[i].trend < 0 or 
-                    price_data[-1].trend < 0 and price_data[i].trend > 0):
-                ticks = price_data[-1].trend - price_data[i+1].trend
-                complete_iteration = False
-                break
-        if complete_iteration:
-            ticks = price_data[-1].trend - price_data[0].trend
 
-        if ticks <= -5 or ticks >= 5:
-            self.trend_speed.append(SpeedPoint(ticks, price_data[-1].time))
+
+    # def update_trend_speed(self):
+    #     self.trend_speeding_ticks = 0
+    #     price_data = self.mtr.data_since(5)
+    #     if len(price_data) < 2:
+    #         return
+    #     ticks = 0
+    #     complete_iteration = True
+    #     for i in reversed(range(len(price_data) - 1)):
+    #         if (price_data[-1].trend > 0 and price_data[i].trend < 0 or 
+    #                 price_data[-1].trend < 0 and price_data[i].trend > 0):
+    #             ticks = price_data[-1].trend - price_data[i+1].trend
+    #             complete_iteration = False
+    #             break
+    #     if complete_iteration:
+    #         ticks = price_data[-1].trend - price_data[0].trend
+
+    #     if ticks <= -5 or ticks >= 5:
+    #         self.trend_speed.append(SpeedPoint(ticks, price_data[-1].time))
         
-        if len(self.trend_speed) == 0:
-            return
+    #     if len(self.trend_speed) == 0:
+    #         return
 
-        if (price_data[-1].time - self.time_speed[0].time < self.mtr.prm.primary_look_back_time - 300 and
-                not self._speed_min_time_passed):
-            return
-        self._speed_min_time_passed = True
+    #     if (price_data[-1].time - self.time_speed[0].time < self.mtr.prm.primary_look_back_time - 300 and
+    #             not self._speed_min_time_passed):
+    #         return
+    #     self._speed_min_time_passed = True
             
-        while price_data[-1].time - self.trend_speed[0].time > self.mtr.prm.primary_look_back_time:
-            self.trend_speed.pop(0)
+    #     while price_data[-1].time - self.trend_speed[0].time > self.mtr.prm.primary_look_back_time:
+    #         self.trend_speed.pop(0)
 
-        if 5 <= ticks >= max(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
-            self.trend_speeding_ticks = ticks
-        elif -5 >= ticks <= min(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
-            self.trend_speeding_ticks = ticks
+    #     if 5 <= ticks >= max(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+    #         self.trend_speeding_ticks = ticks
+    #     elif -5 >= ticks <= min(self.trend_speed, key=lambda sp: sp.ticks).ticks * 0.75:
+    #         self.trend_speeding_ticks = ticks
 
 
     def state_str(self):
         output = ""
-        # output += "time_speed:\n"
-        # for sp in self.time_speed:
-        #     output += f"{sp.state_str()}\n"
+        if len(self.time_speeding_points) > 0:
+            output += "time_speed:\n"
+            for sp in self.time_speed:
+                output += f"{sp.state_str()}\n"
 
-        # output += "trend_speed:\n"
-        # for sp in self.trend_speed:
-        #     output += f"{sp.state_str()}\n"
-
-        if self.time_speeding_ticks != 0:
-            output += "time_speeding_ticks: {:+d}\n".format(self.time_speeding_ticks)
-        if self.trend_speeding_ticks != 0:
-            output += "trend_speeding_ticks: {:+d}\n".format(self.trend_speeding_ticks)
+            output += "time_speeding_points:\n"
+            for sp in self.time_speeding_points:
+                output += f"{sp.state_str()}\n"
         return output
 
 
 class SpeedPoint:
-    def __init__(self, ticks, time):
+    def __init__(self, ticks, time, max_jump, changes):
         self.ticks = ticks
         self.time = time
+        self.max_jump = max_jump
+        self.changes = changes
         
 
     def state_str(self):
         output = (
-            f"'ticks': {self.ticks}, "
-            f"'time': {self.time}"
+            "'ticks': {:+d}, "
+            "'time': {}, "
+            "'max_jump': {}, "
+            "'changes': {}"
         )
+        output = output.format(self.ticks, self.time, self.max_jump, self.changes)
         return output
